41c4e6ab417343c04b9ced8c316025122ad0276b
Singhalnw <singhalnwoutlook@gmail.com>
2018-03-28 14:39:44 +0700

    fds

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php // callback.php
 
 $access_token = 'kPSxCs1+RlRfJ0YLssvlvn5lTtrjYv5wl0jRhwEKHLmlSkinRSiSjdKVZOksFXDuYqn+uDUL0on7vbTdzcf77bDuDEMEXfH6T+EfcceaLz7CzKqLidYV74xip+ggv5RIR2ZElqcLxS3EDtKDOmgVyAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '9954cf41a29434e6dbe4d03418393d57';
+$pushID = 'U24df0d6973d5322790daf07a182332b2';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -22,14 +24,28 @@ $replyToken = $event['replyToken'];
 $messages = ['type' => 'text','text' => $text."\n Message: ".$stringText  ];
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/reply';
+
+
+if($stringText == 'ข้อ id'){
+
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($replyToken);
+    $response = $bot->pushMessage($pushID, $textMessageBuilder);
+
+    echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+}else{
+    $data = [
+        'messages' => [$messages],
+        ];
+}
+
 $data = [
-'replyToken' => $replyToken,
-'messages' => [$messages],
-];
-
-
-
-
+    'messages' => [$messages],
+    
+    ];
 
 
 
