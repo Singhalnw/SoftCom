9d590e630d00f882e864f53c6eb3553ad5c8d160
Singhalnw <singhalnwoutlook@gmail.com>
2018-03-29 16:26:03 +0700

    dsfa

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ if (!is_null($events['events']))
 
     foreach ($events['events'] as $event) 
     {
-        for($i = 0; $i <= 10; $i++)
+        for($i = 0; $i <= 100; $i++)
         {
             $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
             $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("99999999999");
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ลอง".$i);
             $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
             echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
