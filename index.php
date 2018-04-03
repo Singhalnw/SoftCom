65bccf6d10721c8befe5cefcd8980337d7d20fae
Singhalnw <singhalnwoutlook@gmail.com>
2018-04-03 14:39:05 +0700

    ผห

--- index.php
+++ index.php
@@ -1,18 +1,63 @@
-<?php // callback.php
+<?php 
 
-require "vendor/autoload.php";
+require "vendor/autoload.php"; //เรียกใช้งานไฟล์ sdk
 
 $access_token = 'kPSxCs1+RlRfJ0YLssvlvn5lTtrjYv5wl0jRhwEKHLmlSkinRSiSjdKVZOksFXDuYqn+uDUL0on7vbTdzcf77bDuDEMEXfH6T+EfcceaLz7CzKqLidYV74xip+ggv5RIR2ZElqcLxS3EDtKDOmgVyAdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '9954cf41a29434e6dbe4d03418393d57';
-$pushID = 'U24df0d6973d5322790daf07a182332b';
+$pushID = 'U24df0d6973d5322790daf07a182332b2';
 $content = file_get_contents('php://input');
-$events = json_decode($content, true);
+$events = json_decode($content, true); //แปลงเป็น json
 
 if (!is_null($events['events'])) 
 {
 
     foreach ($events['events'] as $event) 
     {
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
+        {
+            //GET UserID ที่จะส่ง
+            $text = $event['source']['userId'];
+            //ข้อความที่ส่ง
+            $stringText = $event['message']['text'];
+            //GET Token
+            $replyToken = $event['replyToken'];
+            //$messages = ['type' => 'text','text' => $stringText ];
+
+            // Url ที่จะใช้ติดต่อ
+            $url = 'https://api.line.me/v2/bot/message/reply';
+
+            if($stringText == 'ขอ id')
+            {
+
+                $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+                $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
+                $response = $bot->pushMessage($pushID, $textMessageBuilder);
+
+                echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+            }
+            else
+            {
+          
+            }
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+            //ติดต่อสื่อสารกับ server แบบ https
+            $ch = curl_init();
+            curl_setopt( $ch, CURLOPT_URL, $url );
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            echo $result . "\r\n";
+        }
+        
         if ($event['type'] == 'follow') 
         {
             $text = $event['source']['userId'];
@@ -38,6 +83,7 @@ if (!is_null($events['events']))
             curl_close($ch);
             echo $result . "\r\n";
         }
+
     }
 }
 echo "OK";
