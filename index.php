7e483e895205f212961fb710ee6e8df8422deb3f
Singhalnw <singhalnwoutlook@gmail.com>
2018-04-03 13:49:31 +0700

    ผผผ

--- index.php
+++ index.php
@@ -13,7 +13,41 @@ if (!is_null($events['events'])) //ถ้ามีข้อมูลให้เ
 
     foreach ($events['events'] as $event)
     {
-        //เมื่อมี event เมื่อมี user แอดเพื่อนจะสั่งให้ bot ส่ง id ของ user คนนั้นออกมา (เราจะทำมาเพื่อไว้สำหรับแจ้งเตือนสำหรับ user นั้นๆ)
+        //เมื่อมี event ในการได้รับข้อความจาก user จะสั่งให้ bot ส่ง id ของ user คนนั้นออกมา (เราจะทำมาเพื่อไว้สำหรับแจ้งเตือนสำหรับ user นั้นๆ)
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
+        {
+
+            $text = $event['source']['userId'];
+            $stringText = $event['message']['text'];
+            $replyToken = $event['replyToken'];
+            //$messages = ['type' => 'text','text' => $stringText ];
+            $url = 'https://api.line.me/v2/bot/message/reply';
+
+            if($stringText == 'ขอ id')
+            {
+                $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+                $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
+                $response = $bot->pushMessage($pushID, $textMessageBuilder);
+                echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+            }
+            else{}
+
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            echo $result . "\r\n";
+        }
+        
         if ($event['type'] == 'follow') 
         {
             $text = $event['source']['userId'];
